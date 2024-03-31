2a6a6602fffc8550d4b4c9739aafa067dc17853c
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-03-31 13:14:24 +0530

    all the validation for the form is done

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 1. create react app
 2. configured tailwind css 
+3. Header 
+4. Login Form
+5. Sing In Form
+6. Routing of App
+7. Form Validation and use of useRef Hook
 
 
 # Features : 
@@ -10,7 +15,7 @@
     - Sign In / Sign Up Page 
     - redirect to browser page
 
-- Browser 
+- Browser ( after Authentication)
     - Header 
     - Main Movie 
         - Trailer in Background
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
 import Background from './Background';
+import { checkValidData } from '../utils/validate';
 
 
 export default function Login() {
+
+    const [isSignInForm,setIsSignInForm] = useState(true);
+    const [errorMessage,seterrorMessage] = useState(false);
+    const email =useRef(null);
+    const password =useRef(null);
+    const name =useRef(null);
+
+    const handleButtonClick = () => {
+        const message= checkValidData(name.current?name.current.value.trim():'',email.current.value, password.current.value);
+        seterrorMessage(message)
+        if(message.includes("Name")){
+            name.current.value='';
+            email.current.value = '';
+            password.current.value = '';
+        }
+        else if (message.includes("Email")) {
+            email.current.value = '';
+            password.current.value = '';
+        }
+        else if (message.includes("Password")) {
+            password.current.value = '';
+        }
+    }
+
+    const toggleSignInForm = () =>{
+        setIsSignInForm(!isSignInForm)
+    }
     return (
         <div>
             <Header/>
             <Background/>
-            <form className='w-4/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80'>
-                <h1 className='font-bold text-3xl py-4'>Sing In</h1>
-                <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-600"/>
-                <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-600"/>
-                <button className='p-4 my-4 bg-red-700 w-full rounded-lg font-extrabold'>Sing In</button>
+            <form onSubmit={(e)=>e.preventDefault()} className='w-4/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80'>
+                <h1 className='font-bold text-3xl py-4'>{isSignInForm?"Sign In":"Sign Up"}</h1>
+                {!isSignInForm &&  <input type="text" ref={name} placeholder="Full Name" className="p-4 my-4 w-full bg-gray-600"/>}
+                <input type="text" ref={email} placeholder="Email Address" className="p-4 my-4 w-full bg-gray-600"/>
+                <input type="password" ref={password} placeholder="Password" className="p-4 my-4 w-full bg-gray-600"/>
+                <p className='text-red-500 font-extrabold text-xl'>{errorMessage}</p>
+                <button className='p-4 my-4 bg-red-700 w-full rounded-lg font-extrabold' onClick={handleButtonClick}>{isSignInForm?"Sign In":"Sign Up"}</button>
+                <p> {isSignInForm?"New to Netflix ?":"Already registered ? "} <span className='font-bold underline cursor-pointer' onClick={toggleSignInForm}>{isSignInForm?"Sign up now":"Sing in now"}</span></p>
             </form>
         </div>
     );
